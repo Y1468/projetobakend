fb254c0871726bb0cb9d294f18e080402cffa8b0
Y1468 <Azulaoandrade10@gmail.com>
2024-12-20 14:57:37 -0300

    deploy

--- prisma/migrations/20241220175442_migration001/migration.sql
+++ prisma/migrations/20241220175442_migration001/migration.sql
@@ -1,4 +1,16 @@
 -- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `categories` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
